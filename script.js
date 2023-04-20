8a2bd96bb6e62f700767ba2cc4891b39744ade9c
lucaselais <131222122+lucaselais@users.noreply.github.com>
2023-04-19 23:46:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function copiarTexto() {
+    let textoCopiado = document.getElementById("texto");
+    textoCopiado.select();
+    textoCopiado.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+    alert("Muito obrigado pelo carinho ♥ Chave PIX copiada. Agora basta colar a chave PIX no app do seu banco.");
+}
+
+
+function copiarTexto1() {
+    let textoCopiadoe = document.getElementById("texto1");
+    textoCopiadoe.select();
+    textoCopiadoe.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+    alert("Muito obrigado pelo carinho ♥ Chave PIX copiada. Agora basta colar a chave PIX no app do seu banco.");
+}
+
+function copiarTexto2() {
+    let textoCopiadoee = document.getElementById("texto2");
+    textoCopiadoee.select();
+    textoCopiadoee.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+    alert("Muito obrigado pelo carinho ♥ Chave PIX copiada. Agora basta colar a chave PIX no app do seu banco.");
+}
